9eff8a48b093e80fe44444dcaafd204c3515112f
MCaessens <caessens.marco@gmail.com>
2022-02-03 00:09:14 +0100

    Reformat code

--- WordleCopy.Core/Entities/Word.cs
+++ WordleCopy.Core/Entities/Word.cs
@@ -4,13 +4,15 @@ namespace WordleCopy.Core.Entities;
 
 public class Word : IEntity
 {
-
     public Guid Id { get; set; }
     public string Name { get; set; } = "";
+
     public Word(string name)
     {
         Name = name;
     }
-    public Word () { }
-    
+
+    public Word()
+    {
+    }
 }

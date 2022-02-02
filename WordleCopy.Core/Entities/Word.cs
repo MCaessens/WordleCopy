52bde235a98b04d7bd9667cfd234e23856656add
MCaessens <caessens.marco@gmail.com>
2022-02-02 23:47:28 +0100

    Add word

---
+++ WordleCopy.Core/Entities/Word.cs
@@ -0,0 +1,16 @@
+﻿using WordleCopy.Core.Interfaces;
+
+namespace WordleCopy.Core.Entities;
+
+public class Word : IEntity
+{
+
+    public Guid Id { get; set; }
+    public string Name { get; set; } = "";
+    public Word(string name)
+    {
+        Name = name;
+    }
+    public Word () { }
+    
+}

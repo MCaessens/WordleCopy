89e1b20e89d08ce720c34f48a7be2638cf133a29
MCaessens <caessens.marco@gmail.com>
2022-02-03 00:08:57 +0100

    Add seeding

--- WordleCopy.Infrastructure/Data/WordleDbContext.cs
+++ WordleCopy.Infrastructure/Data/WordleDbContext.cs
@@ -8,13 +8,52 @@ public class WordleDbContext : DbContext
 {
     public DbSet<Word> Words { get; set; }
     public DbSet<SelectedWord> SelectedWords { get; set; }
+
     public WordleDbContext(DbContextOptions<WordleDbContext> options) : base(options)
     {
-        
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+        var words = new Word[]
+        {
+            new Word
+            {
+                Id = Guid.NewGuid(),
+                Name = "Torso",
+            },
+            new Word
+            {
+                Id = Guid.NewGuid(),
+                Name = "Whizz",
+            },
+            new Word
+            {
+                Id = Guid.NewGuid(),
+                Name = "Muzzy",
+            },
+            new Word
+            {
+                Id = Guid.NewGuid(),
+                Name = "Frizz",
+            },
+            new Word
+            {
+                Id = Guid.NewGuid(),
+                Name = "Bizzy",
+            },
+            new Word
+            {
+                Id = Guid.NewGuid(),
+                Name = "Jumbo",
+            },
+            new Word
+            {
+                Id = Guid.NewGuid(),
+                Name = "Jumps",
+            }
+        };
+        modelBuilder.Entity<Word>().HasData(words);
     }
 }
